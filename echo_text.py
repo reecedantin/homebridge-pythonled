8108711b1103bcdf8d2a6ae021f1ba7db3a8433c
reecedantin <reecedantin@yahoo.com>
2017-03-13 19:44:14 -0400

    rgb implemented

--- echo_text.py
+++ echo_text.py
@@ -1,7 +1,20 @@
 import sys
 
-sys.stdout.write("started")
+from neopixel import *
 
-while True:
-    sys.stdout.write(sys.stdin.read(10))
-    sys.stdout.write("recieved")
+
+# LED strip configuration:
+LED_COUNT      = 300     # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+
+
+if __name__ == '__main__':
+        strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+        strip.begin()
+        for i in range(strip.numPixels()):
+                strip.setPixelColor(i, Color(sys.argv[1],sys.argv[2],sys.argv[3]))
+        strip.show()
--- index.js
+++ index.js
@@ -26,12 +26,13 @@ function LEDAccessory(log, config) {
   this.log = log;
   this.service = 'Light';
   this.name = config['name'];
+  this.pyLoc = config['pythonLocation']
 }
 
 LEDAccessory.prototype.setHue = function(hue, callback) {
     console.log('setHue:' + hue);
     currentHue = hue;
-    callback();
+    this.sendRGB(callback()).bind(this);
 }
 
 LEDAccessory.prototype.getHue = function(callback) {
@@ -40,8 +41,17 @@ LEDAccessory.prototype.getHue = function(callback) {
 
 LEDAccessory.prototype.setPowerState = function(state, callback) {
     console.log("setPow:" + state);
-    currentPow = state;
-    callback();
+    if(currentPow != state) {
+        if(state) {
+            currentLev = 100;
+        } else {
+            currentLev = 0;
+        }
+        currentPow = state;
+        this.sendRGB(callback()).bind(this);
+    } else {
+        callback();
+    }
 }
 
 LEDAccessory.prototype.getPowerState = function(callback) {
@@ -51,7 +61,7 @@ LEDAccessory.prototype.getPowerState = function(callback) {
 LEDAccessory.prototype.setSaturation = function(saturation, callback) {
     console.log("setSat:" + saturation);
     currentSat = saturation;
-    callback();
+    this.sendRGB(callback()).bind(this);
 }
 
 LEDAccessory.prototype.getSaturation = function(callback) {
@@ -61,11 +71,50 @@ LEDAccessory.prototype.getSaturation = function(callback) {
 LEDAccessory.prototype.setBrightness = function(brightness, callback) {
     console.log("setBri:" + brightness);
     currentLev = brightness;
-    callback();
+    this.sendRGB(callback()).bind(this);
 }
 
 LEDAccessory.prototype.getBrightness = function(callback) {
     callback(null, currentLev);
+}
+
+LEDAccessory.prototype.sendRGB = function(callback) {
+
+    var r, g, b;
+    var h = currentHue/360;
+    var s = currentSat/100;
+    var l = currentLev/100;
+
+    if(s == 0){
+        r = g = b = l; // achromatic
+    }else{
+        var hue2rgb = function hue2rgb(p, q, t){
+            if(t < 0) t += 1;
+            if(t > 1) t -= 1;
+            if(t < 1/6) return p + (q - p) * 6 * t;
+            if(t < 1/2) return q;
+            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+            return p;
+        }
+
+        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        var p = 2 * l - q;
+        r = hue2rgb(p, q, h + 1/3);
+        g = hue2rgb(p, q, h);
+        b = hue2rgb(p, q, h - 1/3);
+    }
+
+
+    var options = {
+      mode: 'text',
+      args: [Math.round(g * 255), Math.round(r * 255), Math.round(b * 255)]
+    };
+
+    PythonShell.run(pyLoc, options, function (err, results) {
+      if (err) throw err;
+      // results is an array consisting of messages collected during execution
+      callback();
+    });
 }
 
 LEDAccessory.prototype.getServices = function() {
